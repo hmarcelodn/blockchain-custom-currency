4debc0d08cf646f872db55d6263746088b9eebc9
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2020-08-13 10:28:16 -0300

    update transaction

--- src/blockchain/wallet/transaction.js
+++ src/blockchain/wallet/transaction.js
@@ -20,12 +20,7 @@ export class Transaction {
             { amount, address: recipientAddress }
         ]);
 
-        transaction.input = {
-            timestamp: Date.now(),
-            amount: senderWallet.balance,
-            address: senderWallet.publicKey,
-            signature: senderWallet.sign(transaction.outputs)
-        };
+        transaction.input = Transaction.sign(transaction, senderWallet);
 
         return transaction;
     }
@@ -33,5 +28,26 @@ export class Transaction {
     static verify(transaction) {
         const { input: { address, signature }, outputs } = transaction;
         return elliptic.verifySignature(address, signature, outputs);
+    }
+
+    static sign(transaction, senderWallet) {
+        return {
+            timestamp: Date.now(),
+            amount: senderWallet.balance,
+            address: senderWallet.publicKey,
+            signature: senderWallet.sign(transaction.outputs)
+        };
+    }
+
+    update(senderWallet, recipientAddress, amount) {
+        const senderOutput = this.outputs.find((output) => output.address === senderWallet.publicKey);
+
+        if (amount > senderOutput.amount) throw Error(`Amount: ${amount} exceeds balance`);
+
+        senderOutput.amount -= amount;
+        this.outputs.push({ amount, address: recipientAddress });
+        this.input = Transaction.sign(this, senderWallet);
+
+        return this;
     }
 }
