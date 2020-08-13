5c35e897cbcbd02023834a85e453e289a0df1a26
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2020-08-13 12:36:32 -0300

    add memorypool

---
+++ src/blockchain/memoryPool.js
@@ -0,0 +1,12 @@
+export class MemoryPool {
+    constructor() {
+        this.transactions = [];
+    }
+
+    addOrUpdate(transaction) {
+        const txIndex = this.transactions.findIndex(({ id }) => id === transaction.id);
+
+        if (txIndex > 0) this.transactions[txIndex] = transaction;
+        else this.transactions.push(transaction);
+    }
+}
